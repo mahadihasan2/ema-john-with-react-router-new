3ff00c1f2ec9a04df83c7b1a7af33668affe0cd0
mahadihasan2 <mahadihasan125643@gmail.com>
2022-05-26 00:59:58 +0600

    login-work-something-done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const navigate = useNavigate()
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value)
+    }
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+
+    if (user) {
+        navigate('/shop')
+    }
+
+    const handleFromSubmit = event => {
+        event.preventDefault()
+        signInWithEmailAndPassword(email, password)
+
+    }
+
     return (
         <div className='from-container'>
             <div>
                 <h2 className='from-title'>This is LogIn</h2>
-                <form action="">
+                <form action="" onSubmit={handleFromSubmit}>
                     <div className='from-input'>
                         <label htmlFor="email">E-mail</label>
-                        <input type="email" name='email' required />
+                        <input onBlur={handleEmailBlur} type="email" name='email' required />
 
                     </div>
                     <div className="from-input">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name='password' required />
+                        <input onBlur={handlePasswordBlur} type="password" name='password' required />
                     </div>
+
+                    <p style={{ color: 'red' }}>{error?.message}</p>
+                    {
+                        loading && <p>Loading...</p>
+                    }
 
                     <input className='from-submit' type="submit" value='Login' />
                 </form>
